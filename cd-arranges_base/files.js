f390531e5cd949d777faf8cbf45bad806cb72ae0
VasteelXolotl <48246459+VasteelXolotl@users.noreply.github.com>
2019-03-05 13:56:35 +0100

    the last commit related to this for realsies. hopefully

--- cd-arranges_base/files.js
+++ cd-arranges_base/files.js
@@ -1,5 +1,5 @@
 {
-	"patch.js": 119666366,
+	"patch.js": 2304167709,
 	"th09/musiccmt.js": 100926053,
 	"th095/musiccmt.js": 2532905946
 }
--- repo.js
+++ repo.js
@@ -3,7 +3,7 @@
 	"id": "VasteelXolotl",
 	"patches": {
 		"cd-arranges": "Some arranges from ZUN's CDs (lossy)",
-		"cd-arranges_base": "Base files for ZUN arranges",
+		"cd-arranges_base": "Base files for ZUN arranges. This doesn't contain *all* of the arranges, just the ones that fit better in my opinion.",
 		"cd-arranges_lossless": "Some arranges from ZUN's CDs (lossless)"
 	},
 	"servers": [
