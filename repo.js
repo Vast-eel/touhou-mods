41d25661a618754db682fc77e7cdb8de5bb58380
VasteelXolotl <48246459+VasteelXolotl@users.noreply.github.com>
2019-08-26 19:23:30 +0200

    fixed shot types

--- repo.js
+++ repo.js
@@ -6,7 +6,8 @@
 		"cd-arranges_base": "Base files for ZUN arranges. This doesn't contain *all* of the arranges, just the ones that fit better in my opinion.",
 		"cd-arranges_lossless": "Some arranges from ZUN's CDs (lossless)",
 		"reimu_gohei": "Restores Reimu's gohei in games after DDC. Original idea by Pook.",
-		"vx-customshots": "Modifications of shot types I thought were lacking."
+		"vx-customshots": "Modifications of shot types I thought were lacking.",
+		"vx-fixedshots": "Fixes the damage of MarisaB in MoF and YoumuC in WBaWC."
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/VasteelXolotl/VasteelXolotl/master/",
---
+++ vx-fixedshots/patch.js
@@ -0,0 +1,12 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "vx-fixedshots",
+	"servers": [
+		"https://raw.githubusercontent.com/VasteelXolotl/VasteelXolotl/master/vx-fixedshots/",
+		"https://mirrors.thpatch.net/VasteelXolotl/vx-fixedshots/",
+		"http://mirrors.thpatch.net/VasteelXolotl/vx-fixedshots/"
+	],
+	"title": "Fixes the damage of MarisaB in MoF and YoumuC in WBaWC."
+}
