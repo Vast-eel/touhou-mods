0f492c4ea751acfd633b38cf4880ad1ba23da083
VasteelXolotl <48246459+VasteelXolotl@users.noreply.github.com>
2020-07-12 14:34:05 +0200

    removing

--- reimu_gohei/files.js
+++ reimu_gohei/files.js
@@ -1,6 +1,6 @@
 {
 	"patch.js": 410810945,
-	"th15/player/pl00/pl00.png": 1944094491,
-	"th16/player/pl00/pl00.png": 2196469609,
-	"th17/player/pl00/pl00.png": 3343577836
+	"th15/player/pl00/pl00.png": null,
+	"th16/player/pl00/pl00.png": null,
+	"th17/player/pl00/pl00.png": null
 }
